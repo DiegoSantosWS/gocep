f9d8846e485816898188ab0372f464f69338fe52
DiegoSantosWS <tec.infor321@gmail.com>
2019-04-01 21:45:53 -0300

    teste git

--- connection/conexao.go
+++ connection/conexao.go
@@ -13,7 +13,7 @@ var Db *sqlx.DB
 
 //Connection realiza conexão com banco de dados
 func Connection() (err error) {
-	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", "user", "pass", "host", "3306", "dbname")
+	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", "root", "9fc46b3397e0588d", "172.17.0.2", "3306", "cepwsite")
 	Db, err = sqlx.Open("mysql", uri)
 	if err != nil {
 		log.Fatal("[ERRO DB] erro ao conectar com banco de dados: ", err.Error())
